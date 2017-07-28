fa8b3e9cd1fea8a24eefdea85f1188dccd472407
Esther H. Jang <infrared@cs.washington.edu>
2017-07-28 19:05:08 +0800

    changed backhaul number to 500

--- scripts/cron_backhaul_alert.sh
+++ scripts/cron_backhaul_alert.sh
@@ -4,6 +4,8 @@ checkfile="/home/endaga/alert_statuses/internet_status.txt"
 touch $checkfile
 #checkfile must exist or script won't run. If it doesn't exist, will run next time.
 
+alertnumber=500;
+
 curl ccm.cs.washington.edu > /dev/null 2>&1;
 disconnected=$?;
 prev_disconnected=$(cat $checkfile);
@@ -19,13 +21,13 @@ then
 elif [ $disconnected -gt 0 ] && [ "$prev_disconnected" -eq 0 ] 
 then
 #        echo Disconnected alert run;
-	python /usr/share/freeswitch/scripts/alert.py 700;
+	python /usr/share/freeswitch/scripts/alert.py "$alertnumber";
 	echo 1 > $checkfile
 #        echo checkfile 1 written
 elif [ $disconnected -eq 0 ] && [ "$prev_disconnected" -eq 1 ]
 then
 #        echo Reconnected alert run;
-	python /usr/share/freeswitch/scripts/alert.py 700;
+	python /usr/share/freeswitch/scripts/alert.py "$alertnumber";
 	echo 0 > $checkfile
 #        echo checkfile 0 written
 else
--- scripts/cron_temp_alert.sh
+++ scripts/cron_temp_alert.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/python
 
+alertnumber=400
+
 temp=$(</sys/class/thermal/thermal_zone0/temp)
 warning=$[$temp>=70000]
 if (($warning))
 then
-	python /usr/share/freeswitch/scripts/alert.py 400
+	python /usr/share/freeswitch/scripts/alert.py "$alertnumber"
 fi
