8c256b0301d6c8c1914717b60934e30f01001786
Esther H. Jang <infrared@cs.washington.edu>
2017-08-13 19:09:16 +0800

    fixed voltage reading to skip serial read errors

--- scripts_27/read_voltage_arduino.py
+++ scripts_27/read_voltage_arduino.py
@@ -8,14 +8,25 @@ ardSerial = serial.Serial(SERIAL_PORT, 9600)
 prevVoltage = 0
 steadyCount = 0
 totalCount = 0
+readSuccess = False
 
 while True:
     totalCount += 1
-    rawVoltage = ardSerial.readline() 
-    readVoltage = rawVoltage.strip()
-    voltage = float(readVoltage)
+    while readSuccess == False:
+        rawVoltage = ardSerial.readline() 
+        readVoltage = rawVoltage.strip()
+        readSuccess = True
+        try:
+            voltage = float(readVoltage)
+        except:
+            readSuccess = False
+
+    readSuccess = False
+
     if voltage == prevVoltage:
         steadyCount += 1
+    else:
+        steadyCount = 0
     if steadyCount >= 4:
         finalVoltage = voltage
         break
@@ -26,4 +37,4 @@ while True:
         prevVoltage = voltage
 
 ardSerial.close()
-print finalVoltage
+print(finalVoltage)
