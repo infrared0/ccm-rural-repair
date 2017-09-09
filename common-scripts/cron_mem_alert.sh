1e369379b22f28d929b2de534fbdfc2be6090ee4
Esther H. Jang <infrared@cs.washington.edu>
2017-09-09 09:35:09 +0800

    note to self do not commit code that does not work due to changes during testing!!!

--- common-scripts/cron_mem_alert.sh
+++ common-scripts/cron_mem_alert.sh
@@ -11,7 +11,7 @@ lowmemory=2000; # in kb
 
 memloc="/proc/meminfo"
 memfree=$(cat $memloc | grep "MemAvailable" | awk '{print $2}')
-memfree=1500
+#memfree=1500
 memmb=$(echo "$memfree/1000" | bc)
 
 warningmsg="BABALA: MABABA MEMORY NG SYSTEM. $memmb Mb na lamang ang natitira sa memory ng BTS computer. Magbura ng ilang di mahalagang bagay upang magkaron ng sapat na memory."
