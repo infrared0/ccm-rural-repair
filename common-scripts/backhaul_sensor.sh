1e7661a60844cc70a166ce281dfa2add94f0a6b3
Esther Jang <jang.esther@gmail.com>
2018-05-07 13:35:21 -0700

    backhaul sensor debug

--- common-scripts/backhaul_sensor.sh
+++ common-scripts/backhaul_sensor.sh
@@ -3,7 +3,9 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/pytho
 
 #config
 
-alertscript=/usr/share/freeswitch/scripts/write_state_arduino.py;
+#alertscript=/usr/share/freeswitch/scripts/write_state_arduino.py;
+
+alertscript=/home/infrared/Research/ccm-rural-repair/common-scripts/write_state_arduino.py;
 serialport=/dev/ttyACM0;
 prev_disconnected=0;
 disconnected=0;
@@ -13,7 +15,7 @@ do
 	# test backhaul
 	curl ccm.cs.washington.edu > /dev/null 2>&1
 	disconnected=$?;
-	echo "$disconnected";
+	echo Current State "$disconnected";
 
 	if [ -z "$prev_disconnected" ]
 	then
@@ -22,13 +24,14 @@ do
 	then
 	    echo Newly disconnected;
 	    python "$alertscript" "$serialport" "$disconnected";
-	elif [ $disconnected -eq 0 ] && [ "$prev_disconnected" -eq 1 ]
+	elif [ $disconnected -eq 0 ] && [ "$prev_disconnected" -gt 0 ]
 	then
 	    echo Newly reconnected;
 	    python "$alertscript" "$serialport" "$disconnected";
 	else
 	    echo same state;
 	fi
+	prev_disconnected="$disconnected";
 
 	sleep 10
 done
