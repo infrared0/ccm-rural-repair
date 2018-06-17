e3fc1b080ae042c4428787e0503225352a63e0dc
pgarrison <philipmg@cs.washington.edu>
2018-06-17 02:55:31 -0700

    [website] Banner image, navbar updates

---
+++ website/navbar.js
@@ -0,0 +1,16 @@
+window.init = function() {
+    var navElems = [ { name: 'Home', url: '/index.html' },
+                     { name: 'People & Places', url: '/people.html' },
+                     { name: 'Background', url: '/background.html' },
+                     { name: 'Mozilla Challenge', url: '/mozilla.html' } ];
+    for(var i in navElems) {
+        var e = navElems[i];
+        var url = window.location.href;
+        if(url.indexOf(e.url) >= 0) {
+            $('#nav').append('<div id="active-nav">' + e.name + '</div>');
+        } else {
+            $('#nav').append('<a href="' + e.url + '">' + e.name + '</a>');
+        }
+    }
+}
+$(document).ready(window.init);
