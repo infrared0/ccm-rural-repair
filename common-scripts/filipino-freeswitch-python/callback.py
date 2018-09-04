98e0a9fd79c5345a9f4d708d25fbf4a1d2ee32be
Esther Jang <jang.esther@gmail.com>
2018-09-03 19:13:06 -0700

    added param to originate

--- common-scripts/filipino-freeswitch-python/callback.py
+++ common-scripts/filipino-freeswitch-python/callback.py
@@ -7,9 +7,9 @@ from ESL import *
 def python_callback(dial_number, recipient, play_fn):
     con = ESLconnection("127.0.0.1", "8021", "ClueCon")
     if con.connected():
-        con.api(str("originate {origination_call_id_name=%s,origination"
+        con.api(str("originate {play_fn=%s,origination_call_id_name=%s,origination"
             "_caller_id_number=%s}sofia/internal/%s@127.0.0.1:"
-            "5060 &echo" % (recipient, play_fn, dial_number)))
+            "5060 &echo" % (play_fn, recipient, recipient, dial_number)))
     else: 
         Exception("ESL Connection Failed")
 
